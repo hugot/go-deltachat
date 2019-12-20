5cbc492a611c83237704fe7a883618a84bfe24a2
Hugo Thunnissen <hugo@hugot.nl>
2019-12-20 10:11:17 +0100

    Remove incorrect comment

--- deltachat/godeltachat.c
+++ deltachat/godeltachat.c
@@ -3,10 +3,6 @@
 extern uintptr_t godeltachat_eventhandler_proxy(dc_context_t* context, int event,
                              uintptr_t data1, uintptr_t data2);
 
-// normally you will have to define function or variables
-// in another separate C file to avoid the multiple definition
-// errors, however, using "static inline" is a nice workaround
-// for simple functions like this one.
 uintptr_t godeltachat_eventhandler(dc_context_t* context, int event,
                              uintptr_t data1, uintptr_t data2)
 {
