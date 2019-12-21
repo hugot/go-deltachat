3acd7c93e07c8c4fdf99eb26775af28b2b961b76
Hugo Thunnissen <hugo@hugot.nl>
2019-12-21 19:39:54 +0100

    Implement dc_lot_t wrapper type

--- deltachat/lot.go
+++ deltachat/lot.go
@@ -7,3 +7,31 @@ import "C"
 type Lot struct {
 	lot *C.dc_lot_t
 }
+
+func (l *Lot) GetID() uint32 {
+	return uint32(C.dc_lot_get_id(l.lot))
+}
+
+func (l *Lot) GetState() int {
+	return int(C.dc_lot_get_state(l.lot))
+}
+
+func (l *Lot) GetText1() string {
+	return cStringToGo(C.dc_lot_get_text1(l.lot))
+}
+
+func (l *Lot) GetText1Meaning() int {
+	return int(C.dc_lot_get_text1_meaning(l.lot))
+}
+
+func (l *Lot) GetText2() string {
+	return cStringToGo(C.dc_lot_get_text2(l.lot))
+}
+
+func (l *Lot) GetTimestamp() int64 {
+	return int64(C.dc_lot_get_timestamp(l.lot))
+}
+
+func (l *Lot) Unref() {
+	C.dc_lot_unref(l.lot)
+}
