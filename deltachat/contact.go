325055176dbb24544b83e41d4cfb57a14e1a4d9c
Hugo Thunnissen <hugo@hugot.nl>
2019-12-22 13:43:38 +0100

    Implement methods for dc_contact_t type wrapper Contact

--- deltachat/contact.go
+++ deltachat/contact.go
@@ -6,3 +6,43 @@ import "C"
 type Contact struct {
 	contact *C.dc_contact_t
 }
+
+func (c *Contact) GetAddress() string {
+	return dcStringToGo(C.dc_contact_get_addr(c.contact))
+}
+
+func (c *Contact) GetColor() uint32 {
+	return uint32(C.dc_contact_get_color(c.contact))
+}
+
+func (c *Contact) GetDisplayName() string {
+	return dcStringToGo(C.dc_contact_get_display_name(c.contact))
+}
+
+func (c *Contact) GetFirstName() string {
+	return dcStringToGo(C.dc_contact_get_first_name(c.contact))
+}
+
+func (c *Contact) GetID() uint32 {
+	return uint32(C.dc_contact_get_id(c.contact))
+}
+
+func (c *Contact) GetName() string {
+	return dcStringToGo(C.dc_contact_get_name(c.contact))
+}
+
+func (c *Contact) GetNameAndAddress() string {
+	return dcStringToGo(C.dc_contact_get_name_n_addr(c.contact))
+}
+
+func (c *Contact) GetProfileImage() string {
+	return dcStringToGo(C.dc_contact_get_profile_image(c.contact))
+}
+
+func (c *Contact) IsBlocked() bool {
+	return int(C.dc_contact_is_blocked(c.contact)) > 0
+}
+
+func (c *Contact) Unref() {
+	C.dc_contact_unref(c.contact)
+}
